af45f640f50715f4dd0da610a97de25ee2e169e2
Mykhailo Grushko <mykhailo.grushko@protonmail.com>
2024-02-02 12:33:54 +0100

    singular operations work, but when there are multiple everything gets weird

--- javascript.js
+++ javascript.js
@@ -21,11 +21,11 @@ class Calculator {
     switch (value) {
       case "=":
         this.currentString = this.currentString.concat(value);
-        const inputCorrector = new InputCorrector(this.currentString);
-        this.currentString = inputCorrector.correct();
+        //const inputCorrector = new InputCorrector(this.currentString);
+        //this.currentString = inputCorrector.correct();
         this.lastInputed.innerHTML = this.currentString;
         this.currentString = "";
-        this.calculate(this.lastInputed.innerHTML);
+        this.currentString = this.calculate(this.lastInputed.innerHTML);
         break;
       case "C":
         this.lastInputed.innerHTML = "";
@@ -42,30 +42,173 @@ class Calculator {
   }
 
   calculate(expression) {
-    const priority = [["("], [")"], ["!", "√"], ["^"], ["*", "/"], ["+", "-"]];
-    const operationExecutionOrder = this.createOperationExecutionOrder(expression, priority);
-
+    let expressionArray = [...expression].slice(0, -1);
+    expressionArray = this.identifyNumbers(expressionArray);
+    expressionArray = this.correctForCalculation(expressionArray);
+    let operationExecutionOrder = this.createOperationExecutionOrder(expressionArray);
+    for (let i = 0; i < operationExecutionOrder.length; i++) {
+      if (expressionArray.length === 1) {
+        break;
+      }
+      expressionArray = this.calculateOperation(operationExecutionOrder[i], expressionArray)[0];
+      console.log(expressionArray);
+      operationExecutionOrder = this.operationMover(operationExecutionOrder, i, this.calculateOperation(operationExecutionOrder[i], expressionArray)[1]);
+    }
+    console.log(expressionArray[0]);
+    return expressionArray[0];
+  }
+  
+  calculateOperation(operationNumber, expressionArray) {
+    let result = 0;
+    let removedArrayItems = 0;
+    switch(expressionArray[operationNumber]) {
+      case "(":
+        console.log("In braces");
+        let i = operationNumber;
+        while (expressionArray[i] !== ")") {
+          i++;
+        }
+        let resultInParentheseese = this.calculate(expressionArray.slice(operationNumber + 1, i+1));
+        console.log("rip: " + resultInParentheseese);
+        removedArrayItems = i+2-operationNumber;
+        result = expressionArray.splice(operationNumber, i+1, resultInParentheseese);
+        break;
+      case "!":
+        removedArrayItems = 1;
+        let opResult = 1;
+        if (expressionArray[operationNumber-1] !== 0) {
+          for (let i = 1; i < expressionArray[operationNumber - 1] + 1; i++) {
+            opResult *= i;
+          }
+        }
+        result = expressionArray.splice(operationNumber-1, operationNumber + 1, opResult);
+        break;
+      case "√":
+        if (expressionArray(operationNumber+1) < 0) {
+          result = "Cannot make the calculation";
+        }
+        removedArrayItems = 1;
+        result = expressionArray.splice(operationNumber, operationNumber + 2, Math.sqrt(expressionArray[operationNumber+1]));
+        break;
+      case "*":
+        removedArrayItems = 2;
+        result = expressionArray.splice(operationNumber - 1, operationNumber + 2, expressionArray[operationNumber-1] * expressionArray[operationNumber+1]);
+        console.log(expressionArray);
+        break;
+      case "/":
+        removedArrayItems = 2;
+        result = expressionArray.splice(operationNumber - 1, operationNumber + 2, expressionArray[operationNumber-1] / expressionArray[operationNumber+1]);
+        break;
+      case "+":
+        removedArrayItems = 2;
+        result = expressionArray.splice(operationNumber - 1, operationNumber + 2, expressionArray[operationNumber-1] + expressionArray[operationNumber+1]);
+        console.log("addition:" + result);
+        break;
+      case "-":
+        removedArrayItems = 2;
+        result = expressionArray.splice(operationNumber - 1, operationNumber + 2, expressionArray[operationNumber-1] - expressionArray[operationNumber+1]);
+        break;
+      case "^":
+        removedArrayItems = 2;
+        result = expressionArray.splice(operationNumber - 1, operationNumber + 2, expressionArray[operationNumber-1] ** expressionArray[operationNumber+1]);
+        break;
+    }
+    console.log("result" + result);
+    return [result, removedArrayItems];
   }
 
-  createOperationExecutionOrder(expression, priority) {
-    const operationExecutionOrder = [[], [], [], [], [], []];
+  operationMover(operationExecutionOrder, operationNumber, numberToTheLeft) {
+    console.log("Pre Mover: " + operationExecutionOrder);
+    for (let i = operationNumber + 1; i < operationExecutionOrder.length; i++) {
+      operationExecutionOrder[i] -= numberToTheLeft;
+    }
+    console.log("Operation Mover Exec Order: " + operationExecutionOrder + ", " + numberToTheLeft);
+    return operationExecutionOrder;
+  }
+  
+  createOperationExecutionOrder(expressionArray) {
+    const priority = [["("], ["!", "√"], ["^"], ["*", "/"], ["+", "-"]];
+    let operationExecutionOrder = [];
 
-    for (let charPos = 0; charPos < expression.length; charPos++) {
+    for (let charPos = 0; charPos < expressionArray.length; charPos++) {
       for (let i = 0; i < priority.length; i++) {
         for (let j = 0; j < priority[i].length; j++) {
-          if (expression[charPos] === priority[i][j]) {
-            operationExecutionOrder[i].push(charPos);
+          if (expressionArray[charPos] === priority[i][j]) {
+            operationExecutionOrder.push(charPos);
             break; // Break out of the inner loop once a match is found
           }
         }
       }
     }
 
+    operationExecutionOrder = this.parenthesisExecutionOrderReverser(expressionArray, operationExecutionOrder);
+    console.log(operationExecutionOrder);
     return operationExecutionOrder;
   }
 
-  calculateOperation(operation) {
+  parenthesisExecutionOrderReverser(expressionArray, operationExecutionOrder) {
+    let parenthesisNumber = 0;
+    for (let i of expressionArray) {
+      if (i === "(") {
+        parenthesisNumber++;
+      }
+    }
+    if (parenthesisNumber > 1) {
+      let portionToReverse = operationExecutionOrder.slice(0, parenthesisNumber);
+      portionToReverse.reverse();
+      operationExecutionOrder.splice(0, portionToReverse.length, ...portionToReverse);
+    }
+    return operationExecutionOrder;
+  }
 
+  identifyNumbers(expressionArray) {
+    const operators = ["(", ")", "!", "√", "^", "*", "/", "+", "-"];
+    let number = null;
+    let numIsFloat = false;
+    let afterDot = 0;
+    let outputArray = [];
+    for (let i = 0; i < expressionArray.length; i++) {
+      if (operators.includes(expressionArray[i])) {
+        if (number !== null) {
+          outputArray.push(number);
+          numIsFloat = false;
+          afterDot = 0;
+          number = null;
+        }
+        outputArray.push(expressionArray[i]);
+      } else {
+        if (expressionArray[i] === ".") {
+          numIsFloat = true;
+          number = parseFloat(number);
+        } else {
+          if (number != null) {
+            if (!numIsFloat) {
+              number *= 10;
+              number += parseInt(expressionArray[i]);
+            } else {
+              afterDot++;
+              number += parseInt(expressionArray[i]) / (10*afterDot);
+            }  
+          } else {
+            number = parseInt(expressionArray[i]);
+          }
+        }
+      }
+    }
+    if (number !== null) {
+      outputArray.push(numIsFloat ? parseFloat(number) : parseInt(number));
+    }
+    return outputArray;
+  }
+  
+  correctForCalculation(expressionArray) {
+    const operators = ["(", ")", "!", "√", "^", "*", "/", "+", "-"];
+    for (let i = 0; i < expressionArray.length; i++) {
+      if (expressionArray[i] === "√" && !operators.includes(expressionArray[i-1])) {
+        expressionArray.splice(i, 0, "*");
+      }
+    }
+    return expressionArray;
   }
 }
 
